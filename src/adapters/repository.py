da521830609d4a0501ab5b22eea55af0009b4138
FlavClover <zaur_from_flava@icloud.com>
2022-09-11 16:18:13 +0300

    Добавлен класс по паттерну 'Репозиторий' для работы с пользователями

---
+++ src/adapters/repository.py
@@ -0,0 +1,31 @@
+import abc
+from src.domain import model
+from sqlalchemy.orm import Session
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, obj):
+        pass
+
+    @abc.abstractmethod
+    def get_by_id(self, id_obj):
+        pass
+
+    @abc.abstractmethod
+    def all(self):
+        pass
+
+
+class UserRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, obj):
+        self.session.add(obj)
+
+    def get_by_id(self, id_obj):
+        return self.session.get(model.User, id_obj)
+
+    def all(self):
+        return self.session.query(model.User).all()
